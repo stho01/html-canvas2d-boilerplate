baf4acc460c0f1b02f191ed2899634fa7a60e693
Sten Marius <sten.marius.holmen@gmail.com>
2020-04-09 09:10:01 +0200

    Upgrade webpack to v4.42.1

--- src/geometry/Line.ts
+++ src/geometry/Line.ts
@@ -7,7 +7,7 @@ export class Line {
     //********************************************
     //** attributes:
     //********************************************
-    
+
     private _p1     : Vector2D;
     private _p2     : Vector2D;
     private _dir    : Vector2D;
@@ -16,18 +16,18 @@ export class Line {
     //********************************************
     //** ctor:
     //********************************************
-    
+
     constructor(p1: Point|Vector2D, p2: Point|Vector2D) {
         this._p1     = new Vector2D(p1.x, p1.y);
         this._p2     = new Vector2D(p2.x, p2.y);
         this._dir    = this._p2.subtract(this._p1);
-        this._length = this._dir.lenght();
+        this._length = this._dir.length();
     }
-    
+
     //********************************************
     //** getters:
     //********************************************
-    
+
     get p1()        : Vector2D  { return this._p1; }
     get p2()        : Vector2D  { return this._p2; }
     get direction() : Vector2D  { return this._dir; }
